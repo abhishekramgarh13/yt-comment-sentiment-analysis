88f60f9ce2a5acd5d7522925e25fd58806fbfeac
Abhishek Kumar <abhishekramg13@gmail.com>
2024-12-03 18:28:08 +0530

    added docker file

--- flask_app/app.py
+++ flask_app/app.py
@@ -12,8 +12,7 @@ import joblib
 import re
 import pandas as pd
 import nltk
-nltk.download('stopwords')
-nltk.download('wordnet')
+
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from mlflow.tracking import MlflowClient
